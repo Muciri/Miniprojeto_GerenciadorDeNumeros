cb6a60cea37ffce93a6c55edffdf9ad3053e9079
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2026-08-18 20:26:11 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Gerenciador de Números em Go
+
+Atividade de nivelamento em Go da disciplina de **Programação Distribuída**. Com objetivo de praticar conceitos básicos da linguagem, como variáveis, condicionais, loops, funções, slices, múltiplos retornos e tratamento de erros.
+
+## Funcionalidades
+
+* Adicionar números à lista
+* Listar números armazenados
+* Remover número por índice
+* Calcular mínimo, máximo e média
+* Realizar divisão de doius numeros
+* Limpar a lista
+* Listar apenas números pares
+* Ordenar a lista em ordem crescente
+* Ordenar a lista em ordem decrescente
+* exportar lista para arquivo texto
+
+## Como executar
+
+```bash
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,181 @@
+package main
+import (
+    "fmt"
+    "errors"
+    "slices"
+    "sort"
+)
+
+var lista []int
+
+func adicionarNumero(numero int) error {
+    if numero < 0 {
+        return errors.New("numero tem que ser maior que 0")
+    }
+	
+	lista = append(lista, numero)
+	return nil
+}
+
+func listarNumeros() []int  {
+	return lista
+}
+
+func listarNumerosPares() []int  {
+	var listaPar []int
+	
+	for _, numero := range lista {
+	    if numero%2 == 0 {
+	        listaPar = append(listaPar, numero)
+	    }
+	}
+	
+	return listaPar
+}
+
+func removerNumero(indice int) {
+	if indice < 0 || indice >= len(lista) {
+		return
+	}
+	
+	lista = append(lista[:indice], lista[indice+1:]...)
+}
+
+func calcularEstatisticas() (int, int, float64) {
+    if len(lista) == 0 {
+        return 0, 0, 0.0
+    }
+	
+	var minimo int = lista[0]
+	var maximo int = lista[0]
+	var media float64 = 0.0
+	
+	for _, numero := range lista {
+	    if numero < minimo {
+	        minimo = numero
+	    }
+	    
+	    if numero > maximo {
+	        maximo = numero
+	    }
+	    
+	    media += float64(numero)
+	}
+	media = media / float64(len(lista))
+
+    return minimo, maximo, media
+}
+
+func dividir(numero1,numero2 float64) (float64, error) {
+	if numero2 == 0 {
+	    return 0, errors.New("não pode dividir por 0")
+	}
+
+	return numero1 / numero2, nil
+}
+
+func limparLista() {
+	lista = lista[:0]
+}
+
+func ordenarCrescente() {
+    sort.Ints(lista)
+}
+
+func ordenarDecrescente() {
+    sort.Ints(lista)
+    slices.Reverse(lista)
+}
+
+func main() {
+	var escolha int
+
+	fmt.Println("Gerenciador de Números")
+
+	for {
+	fmt.Print(`
+===== MENU =====
+1  - Adicionar Número
+2  - Listar Números
+3  - Remover por Índice
+4  - Estatísticas
+5  - Divisão De Numeros
+6  - Limpar Lista
+7  - Listar Numeros Pares
+8  - Ordenar a Lista em Ordem Crescente
+9  - Ordenar a Lista em Ordem Decrescente
+0  - Sair
+Escolha uma opção: `)
+
+		fmt.Scan(&escolha)
+		fmt.Println("==========")
+
+		switch escolha {
+    	    case 1:
+        		var numero int
+        		fmt.Println("escreva o número: ")
+    			fmt.Scan(&numero)
+    			
+                err := adicionarNumero(numero)
+            
+                if err != nil {
+                    fmt.Println(err)
+                } else {
+                    fmt.Println("Número adicionado: ", numero)
+                }
+    
+    		case 2:
+    			fmt.Println("lista: ",listarNumeros())
+    
+    		case 3:
+    			var indice int
+    			fmt.Println("escreva o número: ")
+    			fmt.Scan(&indice)
+    			removerNumero(indice)
+    
+    		case 4:
+    			minimo, maximo, media := calcularEstatisticas()
+    			fmt.Println("estatísticas:")
+    		    fmt.Println("minimo: ",minimo)
+    		    fmt.Println("maximo: ",maximo)
+    		    fmt.Println("media: ",media)
+    
+    		case 5:
+    			var numero1 float64
+    			var numero2 float64
+    
+    			fmt.Println("escreva o primeiro número: ")
+    			fmt.Scan(&numero1)
+    			fmt.Println("escreva o segundo número: ")
+    			fmt.Scan(&numero2)
+    
+                resultado, erro := dividir(numero1, numero2)
+    
+    			if erro == nil {
+    			fmt.Println(resultado)
+    			} else {
+    				fmt.Println(erro)
+    			}
+    
+    		case 6:
+    			limparLista()
+    		case 7:
+    		    fmt.Println(listarNumerosPares())
+    		    
+    		case 8:
+    		    ordenarCrescente()
+    		    fmt.Println("lista ordenada em ordem crescente")
+    		    
+    		case 9:
+    		    ordenarDecrescente()
+    		    fmt.Println("lista ordenada em ordem decrescente")
+    
+    		case 0:
+    			fmt.Println("Saindo...")
+    			return
+    
+    		default:
+    			fmt.Println("Opção inválida!")
+		}
+	}
+}
