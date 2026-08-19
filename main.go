e3220db10a6b70bb9877f93a71200660b85b362b
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2026-08-19 18:27:10 -0300

    feat: adiciona limpeza de terminal e funcionalidade do arquivo texto

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
     "errors"
     "slices"
     "sort"
+    "os"
+    "strconv"
 )
 
 var lista []int
@@ -12,70 +14,71 @@ func adicionarNumero(numero int) error {
     if numero < 0 {
         return errors.New("numero tem que ser maior que 0")
     }
-	
-	lista = append(lista, numero)
-	return nil
+   
+    lista = append(lista, numero)
+    return nil
 }
 
 func listarNumeros() []int  {
-	return lista
+    return lista
 }
 
 func listarNumerosPares() []int  {
-	var listaPar []int
-	
-	for _, numero := range lista {
-	    if numero%2 == 0 {
-	        listaPar = append(listaPar, numero)
-	    }
-	}
-	
-	return listaPar
+    var listaPar []int
+   
+    for _, numero := range lista {
+        if numero%2 == 0 {
+            listaPar = append(listaPar, numero)
+        }
+    }
+   
+    return listaPar
 }
 
 func removerNumero(indice int) {
-	if indice < 0 || indice >= len(lista) {
-		return
-	}
-	
-	lista = append(lista[:indice], lista[indice+1:]...)
+    if indice < 0 || indice >= len(lista) {
+        return
+    }
+   
+    lista = append(lista[:indice], lista[indice+1:]...)
 }
 
 func calcularEstatisticas() (int, int, float64) {
     if len(lista) == 0 {
         return 0, 0, 0.0
     }
-	
-	var minimo int = lista[0]
-	var maximo int = lista[0]
-	var media float64 = 0.0
-	
-	for _, numero := range lista {
-	    if numero < minimo {
-	        minimo = numero
-	    }
-	    
-	    if numero > maximo {
-	        maximo = numero
-	    }
-	    
-	    media += float64(numero)
-	}
-	media = media / float64(len(lista))
+   
+    var minimo int = lista[0]
+    var maximo int = lista[0]
+    var media float64 = 0.0
+   
+    for _, numero := range lista {
+        if numero < minimo {
+            minimo = numero
+        }
+       
+        if numero > maximo {
+            maximo = numero
+        }
+       
+        media += float64(numero)
+    }
+    media = media / float64(len(lista))
+
 
     return minimo, maximo, media
 }
 
 func dividir(numero1,numero2 float64) (float64, error) {
-	if numero2 == 0 {
-	    return 0, errors.New("não pode dividir por 0")
-	}
+    if numero2 == 0 {
+        return 0, errors.New("não pode dividir por 0")
+    }
 
-	return numero1 / numero2, nil
+    return numero1 / numero2, nil
 }
 
 func limparLista() {
-	lista = lista[:0]
+    lista = lista[:0]
 }
 
 func ordenarCrescente() {
@@ -87,13 +90,39 @@ func ordenarDecrescente() {
     slices.Reverse(lista)
 }
 
+func exportarArquivo() error {
+    arquivo, err := os.Create("lista.txt")
+    if err != nil {
+        return err
+    }
+    
+    defer arquivo.Close()
+
+    fmt.Fprintf(arquivo, "lista: [")
+    for indice, numero := range lista {
+        _, err := fmt.Fprintf(arquivo, strconv.Itoa(numero))
+        if indice != len(lista)-1 {
+            fmt.Fprintf(arquivo, ", ")
+        }
+        if err != nil {
+            return err
+        }
+    }
+    fmt.Fprintf(arquivo, "]")
+
+    return nil
+}
+
+func limparTerminal() {
+    fmt.Print("\033[H\033[2J")
+}
+
 func main() {
-	var escolha int
+    var escolha int
 
-	fmt.Println("Gerenciador de Números")
-
-	for {
-	fmt.Print(`
+    for {
+    fmt.Print(`
+Gerenciador de Números
 ===== MENU =====
 1  - Adicionar Número
 2  - Listar Números
@@ -104,78 +133,90 @@ func main() {
 7  - Listar Numeros Pares
 8  - Ordenar a Lista em Ordem Crescente
 9  - Ordenar a Lista em Ordem Decrescente
+10 - Exportar em arquivo texto
 0  - Sair
 Escolha uma opção: `)
 
-		fmt.Scan(&escolha)
-		fmt.Println("==========")
+        fmt.Scan(&escolha)
+        limparTerminal()
+        fmt.Println("===================================")
 
-		switch escolha {
-    	    case 1:
-        		var numero int
-        		fmt.Println("escreva o número: ")
-    			fmt.Scan(&numero)
-    			
+        switch escolha {
+            case 1:
+                var numero int
+                fmt.Println("escreva o número: ")
+                fmt.Scan(&numero)
+               
                 err := adicionarNumero(numero)
-            
+           
                 if err != nil {
                     fmt.Println(err)
                 } else {
                     fmt.Println("Número adicionado: ", numero)
                 }
-    
-    		case 2:
-    			fmt.Println("lista: ",listarNumeros())
-    
-    		case 3:
-    			var indice int
-    			fmt.Println("escreva o número: ")
-    			fmt.Scan(&indice)
-    			removerNumero(indice)
-    
-    		case 4:
-    			minimo, maximo, media := calcularEstatisticas()
-    			fmt.Println("estatísticas:")
-    		    fmt.Println("minimo: ",minimo)
-    		    fmt.Println("maximo: ",maximo)
-    		    fmt.Println("media: ",media)
-    
-    		case 5:
-    			var numero1 float64
-    			var numero2 float64
-    
-    			fmt.Println("escreva o primeiro número: ")
-    			fmt.Scan(&numero1)
-    			fmt.Println("escreva o segundo número: ")
-    			fmt.Scan(&numero2)
-    
+   
+            case 2:
+                fmt.Println("lista: ",listarNumeros())
+   
+            case 3:
+                var indice int
+                fmt.Println("escreva o número: ")
+                fmt.Scan(&indice)
+                removerNumero(indice)
+   
+            case 4:
+                minimo, maximo, media := calcularEstatisticas()
+                fmt.Println("estatísticas:")
+                fmt.Println("minimo: ",minimo)
+                fmt.Println("maximo: ",maximo)
+                fmt.Println("media: ",media)
+   
+            case 5:
+                var numero1 float64
+                var numero2 float64
+   
+                fmt.Println("escreva o primeiro número: ")
+                fmt.Scan(&numero1)
+                fmt.Println("escreva o segundo número: ")
+                fmt.Scan(&numero2)
+   
                 resultado, erro := dividir(numero1, numero2)
-    
-    			if erro == nil {
-    			fmt.Println(resultado)
-    			} else {
-    				fmt.Println(erro)
-    			}
-    
-    		case 6:
-    			limparLista()
-    		case 7:
-    		    fmt.Println(listarNumerosPares())
-    		    
-    		case 8:
-    		    ordenarCrescente()
-    		    fmt.Println("lista ordenada em ordem crescente")
-    		    
-    		case 9:
-    		    ordenarDecrescente()
-    		    fmt.Println("lista ordenada em ordem decrescente")
-    
-    		case 0:
-    			fmt.Println("Saindo...")
-    			return
-    
-    		default:
-    			fmt.Println("Opção inválida!")
-		}
-	}
+   
+                if erro == nil {
+                fmt.Println(resultado)
+                } else {
+                    fmt.Println(erro)
+                }
+   
+            case 6:
+                limparLista()
+            case 7:
+                fmt.Println(listarNumerosPares())
+               
+            case 8:
+                ordenarCrescente()
+                fmt.Println("lista ordenada em ordem crescente")
+               
+            case 9:
+                ordenarDecrescente()
+                fmt.Println("lista ordenada em ordem decrescente")
+
+            case 10:
+                err := exportarArquivo()
+
+                if err != nil {
+                    fmt.Println("Erro ao exportar:", err)
+                } else {
+                    fmt.Println("Lista exportada com sucesso!")
+                }
+   
+            case 0:
+                fmt.Println("Saindo...")
+                return
+   
+            default:
+                fmt.Println("Opção inválida!")
+        }
+        fmt.Println("===================================")
+    }
 }
